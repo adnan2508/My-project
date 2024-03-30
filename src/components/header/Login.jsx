9de09682ead3eec50df58134cd4d588c1384d80f
adnan2508 <adnandiu2800@gmail.com>
2024-03-31 00:52:02 +0600

    dd

--- src/components/header/Login.jsx
+++ src/components/header/Login.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { getAuth } from "firebase/auth";
+import { getAuth, signInWithPopup } from "firebase/auth";
 import app from '../../firebase.init';
+import { GoogleAuthProvider } from "firebase/auth";
+
 
 const Login = () => {
     const auth = getAuth(app);
+    console.log(app);
+    const provider = new GoogleAuthProvider();
+
+    const handleSignIn = () => {
+        signInWithPopup(auth, provider)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error =>{
+            console.log('error', error.message)
+        })
+    }
+
     return (
         <div>
-            <button>Login</button>
+            <button onClick={handleSignIn}>Google Login</button>
         </div>
     );
 };
